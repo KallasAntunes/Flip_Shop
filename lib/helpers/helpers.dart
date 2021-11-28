c7532fbc63fb51250c60919bbefeea949a4ad733
kallas <kallas.lima@in8.com.br>
2021-11-28 18:43:28 -0300

    Changed api link to cloud

--- lib/helpers/helpers.dart
+++ lib/helpers/helpers.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
-final String api = "http://192.168.2.103:3001";
+final String api = "https://infinite-anchorage-52680.herokuapp.com";
+// final String api = "http://192.168.0.26:3001";
 final Color backgroundDark = Color(0xFF2E3746);
 final Color mainColor = Color(0xFFEF043F);
